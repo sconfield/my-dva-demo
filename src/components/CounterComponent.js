3f28a55efc37146ce3fc2a2747fe9e1b114287d8
sconfield <isconfield@icloud.com>
2017-01-21 21:56:59 +0800

    add ant design for counter

--- src/components/CounterComponent.js
+++ src/components/CounterComponent.js
@@ -2,13 +2,24 @@ import React from 'react';
 import styles from './CounterComponent.css';
 
 import { connect } from 'dva';
+import { Button, Icon, Card, Badge, Row, Col } from 'antd';
 
 function CounterComponent({ dispatch, count }) {
   return (
     <div className={styles.normal}>
-      <h3>Component: CounterComponent { count } </h3>
-      <button key="add" onClick={() => { dispatch({type: 'counter/add'})}}>+</button>
-      <button key="minus" onClick={() => { dispatch({type: 'counter/minus'})}}>-</button>
+      <Card title="Component: CounterComponent" style={{ width: 300 }}>
+        <Row>
+          <Col span={4} offset={4}>
+            <Badge count={ count } />
+          </Col>
+          <Col span={12} offset={4}>
+            <Button type="primary" shape="circle" icon="plus-circle" size="large" key="add"
+              onClick={()=>{ dispatch({ type: 'counter/add' }) }} />
+            <Button shape="circle" icon="minus-circle" size="large" key="minus"
+              onClick={()=>{ dispatch({ type: 'counter/minus' }) }} />
+          </Col>
+        </Row>
+      </Card>
     </div>
   );
 }
